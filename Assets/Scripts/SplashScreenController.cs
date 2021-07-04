8c86e83add6835eb8e7b8dbe7dc1fe2190d27c7c
Castlemark <marc.castells.guell@gmail.com>
2021-07-04 12:28:32 +0200

    Initial commit - splash screen implemented

---
+++ Assets/Scripts/SplashScreenController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DigitalRuby.Tween;
+
+public class SplashScreenController : MonoBehaviour
+{
+  [SerializeField] private RectTransform RTProgressBarFill;
+
+  void Start()
+  {
+    // This will update the progressbar with the values determined by the Tween
+    System.Action<ITween<Vector2>> fillBar = (t) =>
+    {
+      RTProgressBarFill.anchorMax = t.CurrentValue;
+    };
+
+    // This will run once the tween has completed
+    System.Action<ITween<Vector2>> onFillBarCompleted = (t) =>
+    {
+      SceneManager.LoadSceneAsync("MainScene");
+    };
+
+    //We use a tween to simulate the loading of the game
+    TweenFactory.Tween(
+      "FillProgressBar",
+      new Vector2(0.0f, 1.0f),
+      new Vector2(1.0f, 1.0f),
+      5.0f,
+      TweenScaleFunctions.Linear, 
+      fillBar,
+      onFillBarCompleted
+    );
+  }
+}
