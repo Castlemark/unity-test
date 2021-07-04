6da790296fff25966a9cd222b44cfa4bfd8fcaf7
Castlemark <marc.castells.guell@gmail.com>
2021-07-04 19:21:52 +0200

    Added game screens & screen moving logic

---
+++ Assets/Scripts/BackButton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackButton : MonoBehaviour
+{
+  public event Action<int> GameSelected;
+
+  void Awake()
+  {
+    this.GetComponent<Button>().onClick.AddListener(() => { GameSelected?.Invoke(0); });
+  }
+}
---
+++ Assets/Scripts/ScreenMover.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using DigitalRuby.Tween;
+
+public class ScreenMover : MonoBehaviour
+{
+  [SerializeField] private MainScreenController mainScreenController;
+  [SerializeField] private GameObject GOGameScreen1;
+  [SerializeField] private GameObject GOGameScreen2;
+  [SerializeField] private GameObject GOGameScreen3;
+
+  private RectTransform RTScreenContainer;
+
+  void Awake()
+  {
+    RTScreenContainer = this.GetComponent<RectTransform>();
+
+    mainScreenController.GameSelected += (gameScreenIdx) => { onGameScreenSelected(gameScreenIdx); };
+    GOGameScreen1.GetComponentInChildren<BackButton>().GameSelected += (gameScreenIdx) => { onGameScreenSelected(gameScreenIdx); };
+    GOGameScreen2.GetComponentInChildren<BackButton>().GameSelected += (gameScreenIdx) => { onGameScreenSelected(gameScreenIdx); };
+    GOGameScreen3.GetComponentInChildren<BackButton>().GameSelected += (gameScreenIdx) => { onGameScreenSelected(gameScreenIdx); };
+  }
+
+  private void onGameScreenSelected(int gameScreenIdx)
+  {
+    System.Action<ITween<Vector2>> moveToScreen = (t) =>
+      {
+        var anchorMax = RTScreenContainer.anchorMax;
+        var anchorMin = RTScreenContainer.anchorMin;
+        anchorMax.x = t.CurrentValue.y;
+        anchorMin.x = t.CurrentValue.x;
+
+        RTScreenContainer.anchorMax = anchorMax;
+        RTScreenContainer.anchorMin = anchorMin;
+      };
+
+    System.Action<ITween<Vector2>> moveToScreenCompleted = (t) =>
+    {
+      if (t.CurrentValue == Vector2.up)
+      {
+        GOGameScreen1.SetActive(false);
+        GOGameScreen2.SetActive(false);
+        GOGameScreen3.SetActive(false);  
+      }
+    };
+
+    Vector2 curAnchor = new Vector2(RTScreenContainer.anchorMin.x, RTScreenContainer.anchorMax.x);
+    Vector2 targetAnchor = Vector2.up;
+    switch (gameScreenIdx)
+    {
+      case 1:
+        GOGameScreen1.SetActive(true);
+        targetAnchor = Vector2.left;
+        break;
+      case 2:
+        GOGameScreen2.SetActive(true);
+        targetAnchor = Vector2.left;
+        break;
+      case 3:
+        GOGameScreen3.SetActive(true);
+        targetAnchor = Vector2.left;
+        break;
+    }
+
+    TweenFactory.Tween(
+      "MoveToScreen",
+      curAnchor,
+      targetAnchor,
+      1.0f,
+      TweenScaleFunctions.CubicEaseInOut,
+      moveToScreen,
+      moveToScreenCompleted
+      );
+  }
+}
