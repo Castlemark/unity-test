7756209cc46d4577bed0bc6c3dbf4dbe0c7d312b
Castlemark <marc.castells.guell@gmail.com>
2021-07-04 22:21:39 +0200

    Added game 3 and cleaned up code

---
+++ Assets/Scripts/TimestampText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using TMPro;
+
+public class TimestampText : MonoBehaviour
+{
+  private TextMeshProUGUI TMPTimestamp;
+
+  void Awake()
+  {
+    TMPTimestamp = this.gameObject.GetComponent<TextMeshProUGUI>();
+  }
+
+  void Start()
+  {
+    StartCoroutine(getTimestamp());
+  }
+
+  void OnEnable()
+  {
+    StartCoroutine(getTimestamp());
+  }
+
+  // Send web request, then receive, parse and format response to show it in text
+  private IEnumerator getTimestamp()
+  {
+    var request = UnityWebRequest.Get("http://worldtimeapi.org/api/ip");
+    yield return request.SendWebRequest();
+
+    if (request.isHttpError || request.isNetworkError)
+    {
+      Debug.LogError(request.error);
+    }
+    else
+    {
+      string textResponse = request.downloadHandler.text;
+      var timestampResponse = JsonUtility.FromJson<TimestampResponse>(textResponse);
+      TMPTimestamp.text = timestampResponse.datetime.Replace('T', ' ').Replace('-', '/').Split('.')[0];
+    }
+
+  }
+}
